6222c652a84a2267af5290b5e2f4e29df30d3f64
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2019-03-28 23:53:04 +0700

    initial commit

--- session2/homework/2.py
+++ session2/homework/2.py
@@ -1,6 +1,5 @@
 from turtle import *
 
-speed(0)
 color("red")
 
 for i in range(4):
@@ -12,19 +11,15 @@ for i in range(6):
     left(60)
 
 color("blue")
-
-
 left(60)
 forward(100)
 right(120)
 forward(100)
-left(130)
+left(132)
 forward(100)
-left(80)
-forward(100)
-left(60)
-forward(100)
-left(85)
-forward(100)
+
+for i in range(3):
+    left(72)
+    forward(100)
 
 mainloop()
