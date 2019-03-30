340eb0f770e43f047a4b97eb13af5559311a96df
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2019-03-30 13:25:56 +0700

    initial commit

--- session2/homework/6c.py
+++ session2/homework/6c.py
@@ -1,4 +1,8 @@
-x = int(input("Enter a number: "))
-for i in range(x):
-    print('x * ', end="")
+numb = int(input("Enter a number: "))
+
+for i in range(numb):
+    if( i%2==0 ):
+        print("x", end=" ")
+    else:
+        print("*", end=" ")
 print()
